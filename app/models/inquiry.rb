629428f2931d3ee34b9b01236780ae9cf9b4a7f3
mutsumikodama <mutsumikodama+dev@gmail.com>
2015-06-08 21:52:06 +0900

    Form validation

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,6 @@
 class Inquiry < ActiveRecord::Base
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true
+  validates :inquiry, presence: true
 end
